2d65f32a4851e1539f882d2bc8e61f665264b492
Rastko Vukašinović <rastko.vukasinovic@gmail.com>
2020-01-11 00:27:24 +0100

    Adding a joint testing routine (animation still not smooth)

--- sketches/arm_test/arm_test.ino
+++ sketches/arm_test/arm_test.ino
@@ -16,16 +16,16 @@ int motor4_position = 75;
 
 void setup() {
   // Rotation
-//  motor1.attach (9);
+  motor1.attach (9);
 //
 //  // Left motor
-//  motor2.attach (8);
+  motor2.attach (8);
 //
 //  // Right motor
-//  motor3.attach (7);
+  motor3.attach (7);
 //
 //  // Hand motor
-//  motor4.attach (6);
+  motor4.attach (6);
 
   //Direct output to status led
   pinMode(ledPin, OUTPUT);
@@ -43,5 +43,24 @@ void loop() {
   delay(1000);
   digitalWrite(ledPin, LOW);
   delay(1000);
+
+    motor1.write(100);
+    motor2.write(200);
+    motor3.write(100);
+    motor4.write(75);
+    delay(1000);
+//    motor1.write(100);
+//    motor2.write(120);
+//    motor3.write(120);
+//    motor4.write(80);
+//    delay(1000);
+//    motor4.write(50);
+//    delay(1000);
+//    motor1.write(100);
+//    motor2.write(200);
+//    delay(1000);
+//    motor3.write(100);
+//    delay(1000);
+//    motor3.write(90);
 
 }
